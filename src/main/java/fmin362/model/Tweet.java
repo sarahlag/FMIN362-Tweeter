336fcb44003447d14a03a32a4454a1d302a6349e
sarahlag <d_emeter@yahoo.fr>
2013-11-30 19:20:33 +0100

    got tweet persistence

--- src/main/java/fmin362/model/Tweet.java
+++ src/main/java/fmin362/model/Tweet.java
@@ -1,15 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package fmin362.model;
 
-/**
- *
- * @author dem
- */
-public class Tweet {
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+@Entity
+public class Tweet implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    /*
+	@ManyToMany(cascade=CascadeType.ALL)
+	public List<Tag> tags;
+	@Required
+	@ManyToOne
+	@JoinColumn(name="USER_ID")
+	public User author;*/
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.SEQUENCE )
+    private Long id;
+    
+    @Column
+    private String username; // @TODO Add a User model
+    @Column
+    private String tags; // @TODO Add a Tag model
+    @Column
+    private String comment = "";
+    @Column( name = "message_date" )
+    @Temporal( javax.persistence.TemporalType.DATE )
+    private Date date;
+    @Column
+    private String photo_url;
+    @Column
+    private String photo_date;
+    @Column
+    private String photo_place;
+
+    public Tweet() {
+        this.date = new Date();
+        this.comment = "";
+        this.photo_url = "";
+        this.photo_date = "";
+        this.photo_place = "";
+        this.username = "";
+        this.tags = "";
+    }
+    
+    /* ====================
+        Getters and Setters
+       ==================== */
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getPhoto_url() {
+        return photo_url;
+    }
+
+    public void setPhoto_url(String photo_url) {
+        this.photo_url = photo_url;
+    }
+
+    public String getPhoto_date() {
+        return photo_date;
+    }
+
+    public void setPhoto_date(String photo_date) {
+        this.photo_date = photo_date;
+    }
+
+    public String getPhoto_place() {
+        return photo_place;
+    }
+
+    public void setPhoto_place(String photo_place) {
+        this.photo_place = photo_place;
+    }
     
 }
--- src/main/java/fmin362/resources/TweetResource.java
+++ src/main/java/fmin362/resources/TweetResource.java
@@ -1,16 +1,66 @@
 package fmin362.resources;
 
+import fmin362.model.Tweet;
+import java.util.Date;
+import java.util.List;
+import javax.naming.InitialContext;
+import javax.persistence.EntityManager;
+import javax.transaction.UserTransaction;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path( "/tweets" ) // http://localhost:9000/FMIN362-Tweeter/resources/tweets
 public class TweetResource
 {
 
     @GET
-    public String get()
+    @Produces( MediaType.APPLICATION_JSON )
+    public List<Tweet> get()
+            throws Exception
     {
-        return "This is the Tweet Resource";
-    }
+        UserTransaction utx = null;
+        try {
 
+            // Lookup
+            InitialContext ic = new InitialContext();
+            utx = ( UserTransaction ) ic.lookup( "java:comp/UserTransaction" );
+            EntityManager em = ( EntityManager ) ic.lookup( "java:comp/env/persistence/EntityManager" );
+
+            // Transaciton begin
+            utx.begin();
+            em.joinTransaction();
+
+            Tweet newtweet = new Tweet( );
+            newtweet.setUsername("toto");
+            newtweet.setComment("hello world!");
+            em.persist( newtweet );
+
+            Tweet newtweet2 = new Tweet( );
+            newtweet2.setUsername("titi");
+            newtweet2.setComment("bouh");
+            em.persist( newtweet2 );
+            
+            List<Tweet> listtweets = em.createQuery( "select m from Tweet m" ).getResultList(); 
+
+            utx.commit();
+
+            return listtweets;
+
+        } catch ( Exception ex ) {
+
+            try {
+                if ( utx != null ) {
+                    utx.setRollbackOnly();
+                }
+            } catch ( Exception rollbackEx ) {
+                // Impossible d'annuler les changements, vous devriez logguer une erreur,
+                // voir envoyer un email à l'exploitant de l'application.
+            }
+            throw new Exception( ex );
+
+        }
+
+    }
 }
